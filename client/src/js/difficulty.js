4078693e47d82cfc2d87dc1ed3cd04481420a228
Ryan <ohmazio36@gmaiil.com>
2025-03-12 23:22:30 +0800

    Initial commit

---
+++ client/src/js/difficulty.js
@@ -0,0 +1,17 @@
+export const difficulty = [
+    {
+        bossName: "Enemy A",
+        difficultyName:'Tutorial',
+        enemyHp: 50
+    },
+    {
+        bossName: "Enemy B",
+        difficultyName:'Beginer',
+        enemyHp: 100
+    },
+    {
+        bossName: "Enemy c",
+        difficultyName:'Intermediate',
+        enemyHp: 200
+    }
+]
